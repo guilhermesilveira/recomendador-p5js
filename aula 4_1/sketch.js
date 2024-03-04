6b3a10180d199565f8a5e0f4214c2b16bcf8bf44
Guilherme Silveira <guilherme.silveira@gmail.com>
2024-03-04 17:32:10 -0300

    Add files via upload

---
+++ aula 4_1/sketch.js
@@ -0,0 +1,56 @@
+// fantasia, aventura, drama
+
+// a viagem de chihiro, LIVRE, fantasia, aventura
+// paddington, LIVRE, fantasia, aventura
+
+// as aventuras de pi, 10, drama, fantasia, aventura
+// depois da chuva, 10, drama
+// guardiões da galáxia, 12, fantasia, aventura
+// ladrões de bicicleta, 12, drama
+// o menino que descobriu o vento, 14, drama
+
+let campoIdade;
+let campoFantasia;
+
+function setup() {
+  createCanvas(400, 400);
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+}
+
+function draw() {
+  background(220);
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia);
+  text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade, gostaDeFantasia) {
+  if(idade >= 10) {
+    if(idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      if(gostaDeFantasia) {
+        return "As aventuras de pi";
+      } else {
+        return "Depois da chuva";
+      }
+    }
+  } else {
+    if(gostaDeFantasia) {
+      return "A viagem de chihiro";
+    } else {
+      return "O feitiço do tempo";
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
